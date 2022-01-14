774e16a85e8ce09b92b74f691e551f83a8e67d14
Facundo Fontela <facundo.fontela@mangoodev.com>
2022-01-14 10:38:33 -0300

    exercises 3 to 3.8

---
+++ index.js
@@ -0,0 +1,93 @@
+const express = require('express');
+var app = express()
+var morgan = require('morgan')
+app.use(morgan('combined'))
+
+
+const persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+];
+
+app.get("/api/persons", (req, res) => {
+    res.json(persons);
+});
+
+app.get("/api/info", (req, res) => {
+    const info = {
+        amount: `Phonebook has info for ${persons.length} people.`,
+        date: new Date()
+    }
+
+    res.json(info);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const person = persons.find(x => x.id === id);
+
+    if(person) {
+        res.json(person);
+    }
+
+    res.status(404).end();
+});
+
+app.delete("/api/persons/delete/:id", (req, res) => {
+    const id = Number(req.params.id);
+
+    if(persons.some(x => x.id === id)) {
+        persons = persons.filter(x => x.id !== id);
+        res.json(persons);
+    } else {
+        res.status(404).end();
+    }
+});
+
+const getRandomId = ()=> {
+    return Math.floor(Math.random() * 100000);
+}
+
+app.post("/api/persons", (req, res) => {
+   const person = req.body;
+
+   if(!person.name || !person.number) {
+       res.status(404).json({
+           error: "Person must have name and number."
+       });
+   }
+
+   if(persons.find(x => x.name === person.name)) {
+    res.status(404).json({
+        error: "This name is already used."
+    });
+   }
+
+   person.id = getRandomId();
+   persons = persons.concat(person);
+   res.json(person);
+});
+
+
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`App listen on PORT: ${PORT}`)
+});
